429baeb6cd6a66e37ab7d0c1110b51aa9b245162
Navneet Aulakh <navneetaulakh@Navneets-MacBook-Pro.local>
2024-11-28 16:46:54 -0800

    updates

---
+++ .history/src/App_20241128164639.jsx
@@ -0,0 +1,18 @@
+import { useEffect, useState } from 'react'
+import './App.css'
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Countries from '../components/Countries';
+import Details from '../components/Details';
+
+function App() {
+  return (
+    <BrowserRouter basename="COMP-3170-lab07">
+      <Routes>
+        <Route path="/" element={<Countries />} />
+        <Route path="/countries/:cca2" element={<Details />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
